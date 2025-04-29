215c991a439ca922fcacee725bb8d690d2e811ac
Tanguy Lemarie <tlemarie@serlem.com>
2025-04-29 11:09:15 +0200

    Add a script to download all requirements for programs

---
+++ requirements.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install libunwind-dev libnfc-dev           # For the unwind library, allowing us to print clear call stack after a crash
+sudo dnf install libunwind-devel libnfc-devel       # For the NFC library, allowing us to use NFC card readers.
